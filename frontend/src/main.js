2c44c55e6228c413505eafe5d29212bfac5a6c90
celsoprazer <celsocfx@gmail.com>
2026-02-07 18:35:48 -0300

    Setup Sass

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/main.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createApp(App).mount('#app')
